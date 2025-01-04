3d7d01ceefe09440d652fca878f359b2e07b9e2a
Roma <rcemeniuk@gmail.com>
2025-01-04 16:15:59 +0100

    lol

--- main.py
+++ main.py
@@ -1,105 +1,52 @@
-from dataclasses import dataclass
-import math
-from typing import List, Self
-import pygame
+import math as m
+import random as rd
+from typing import Any
+import pygame as pg
+import numpy as np
+import numpy.typing as npt
 
+balls = 10
+radius = 1
+balls_positions: npt.NDArray[np.float64] = np.array(
+    [[float(rd.randint(100, 700)), float(rd.randint(100, 500))] for _ in range(balls)],
+    ndmin=2,
+)
+balls_velocities: npt.NDArray[np.float64] = np.array(
+    [
+        [rd.random() * rd.choice([1, -1]) * 2, rd.random() * rd.choice([1, -1]) * 2]
+        for _ in range(balls)
+    ],
+    ndmin=2,
+)
 
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    @classmethod
-    def from_deg(cls, deg, hypot) -> Self:
-        r = math.radians(deg)
-        x, y = round(math.cos(r) * hypot, 2), round(math.sin(r) * hypot, 2)
-        return cls(x, y)
-
-    def __add__(self, other: Self):
-        return Vector(self.x + other.x, self.y + other.y)
-
-    def __mul__(self, scalar: int):
-        return Vector(self.x * scalar, self.y * scalar)
-
-    def __str__(self):
-        return f"Vector({self.x}, {self.y})"
-
-    def __repr__(self):
-        return self.__str__()
-
-    @property
-    def hypot(self):
-        return (self.x**2 + self.y**2) ** 0.5
-
-    def __matmul__(self, other: Self):
-        return self.x * other.x + self.y * other.y
-
-    def angle_clockwise(self, other: Self):
-        return round(
-            (
-                math.degrees(
-                    math.acos((self @ other) / (abs(self.hypot) * abs(other.hypot)))
-                )
-                if self.y > 0
-                else 360
-                - math.degrees(
-                    math.acos((self @ other) / (abs(self.hypot) * abs(other.hypot)))
-                )
-            ),
-            3,
-        )
-
-    def angle(self, other: Self):
-        return round(
-            math.degrees(
-                math.acos((self @ other) / (abs(self.hypot) * abs(other.hypot)))
-            ),
-            3,
-        )
-
-    def rotated(self, deg):
-        return self.from_deg(self.angle_clockwise(Vector(1, 0)) - deg, self.hypot)
-
-    @property
-    def tuple(self):
-        return (self.x, self.y)
-
-    def __radd__(self, other):
-        if other == 0:
-            return self
-        else:
-            return self.__add__(other)
-
-
-class Decceleration(Vector): ...
-
-
-class ExpressionVector(Vector): ...
-
-
-@dataclass
-class Ball:
-    pos: Vector
-    rad: int
-    forces: List[Vector | Decceleration] | None = None
-
-    @property
-    def rect(self):
-        return pygame.Rect(*self.pos.tuple, self.rad - 1, self.rad - 1)
-
-    def update(self):
-        if self.forces:
-            self.pos = sum([self.pos, sum(self.forces)])
-
-    def draw(self, screen: pygame.Surface):
-        pygame.draw.circle(screen, (255, 255, 255), self.pos.tuple, self.rad)
-
-
-screen = pygame.display.set_mode((500, 500))
-ball = Ball(Vector(250, 250), 10, [Vector(0.01, 0)])
+clock = pg.time.Clock()
+screen = pg.display.set_mode((800, 600))
 
 while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
     screen.fill((0, 0, 0))
-    ball.update()
-    ball.draw(screen)
-    pygame.display.update()
+    gravity = [
+        sum(
+            [
+                (1 / m.dist(p1, p2) ** 3 * (p2 - p1)) * 100
+                for p2 in np.delete(balls_positions, np.where(balls_positions == p1), 0)
+            ],
+        )
+        for p1 in balls_positions
+    ]
+
+    balls_velocities += gravity
+    drag = -0.01 * balls_velocities
+    balls_velocities += drag
+
+    balls_positions += balls_velocities
+    for i, pos in enumerate(balls_positions):
+        # if pos[0] < 0 + radius or pos[0] > 800 - radius:
+        #     balls_velocities[i][0] *= -1
+        # if pos[1] < 0 + radius or pos[1] > 600 - radius:
+        #     balls_velocities[i][1] *= -1
+        pg.draw.circle(screen, (255, 255, 255), tuple(pos), radius)
+    pg.display.flip()
+    clock.tick(60)
